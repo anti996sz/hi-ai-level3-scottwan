3d6097f4e327989dcaac9b13fb84d82323d173c9
Scott Wan <oncwnuKMMQbK62AfJkKp1MmOX8cg@git.weixin.qq.com>
2020-05-02 22:14:47 +0800

    level-1

---
+++ miniprogram/pages/level-1/index.js
@@ -0,0 +1,109 @@
+// miniprogram/pages/level-1/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  chooseImage() {
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'],
+
+      success: chooseResult => {
+        const filePath = chooseResult.tempFilePaths[0]
+        const cloudPath = 'img-to-detect' + filePath.match(/\.[^.]+?$/)[0]
+
+        this.setData({
+          faceImageUrl: filePath,
+          labelList: []
+        })
+
+        // 将图片上传至云存储空间
+        wx.cloud.uploadFile({
+          // 指定上传到的云路径
+          cloudPath,
+          // 指定要上传的文件的小程序临时文件路径
+          filePath,
+          // 成功回调
+          success: res => {
+            // console.log('上传成功', res)
+            wx.cloud.callFunction({
+              name: 'imgLabel',
+              data: {
+                action: 'DetectLabel',
+                cloudPath
+              }
+            }).then(({
+              result
+            }) => {
+              this.setData({
+                labelList: result.RecognitionResult.Labels
+              })
+            })
+          },
+        })
+      },
+    })
+  }
+})
